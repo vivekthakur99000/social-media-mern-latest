045c72923b334963671befd791214a614fc4ee73
vivek thakur <vivekthakur99000@gmail.com>
2025-09-13 15:49:32 +0530

    updated the router dom for projects

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from "./pages/Login"
+import Feed from "./pages/Feed"
+import Messages from "./pages/Messages"
+import ChatBox from './pages/ChatBox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Login/>}>
+        <Route index element={<Feed />} />
+        <Route path='messages' element={<Messages />} />
+        <Route path='messages/:id' element={<ChatBox />} />
+        <Route path='connections' element={<Connections />} />
+        <Route path='discover' element={<Discover />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='profile/:profileId' element={<Profile/>} />
+        <Route path='create-post' element={<CreatePost/>} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
