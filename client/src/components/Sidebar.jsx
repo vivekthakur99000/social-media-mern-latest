dfaba1cb2fc9cd06799eb276239b96321278d342
vivek thakur <vivekthakur99000@gmail.com>
2025-09-16 18:38:45 +0530

    refactor Sidebar component to include user data and improve layout

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { assets } from "../assets/assets";
-import { useNavigate } from "react-router-dom";
+import { assets, dummyUserData } from "../assets/assets";
+import { Link, useNavigate } from "react-router-dom";
 import MenuItems from "./MenuItems";
+import { CirclePlus, LogOut } from "lucide-react";
+import { UserButton, useClerk } from "@clerk/clerk-react";
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const user = dummyUserData;
+  const { signOut } = useClerk();
 
   return (
     <div
@@ -14,9 +17,32 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
       } transition-all duration-300 ease-in-out `}
     >
       <div className="w-full">
-        <img onClick={() => navigate("/")} src={assets.logo} className="w-26 ml-7 my-2 cursor-pointer " alt="" /> 
+        <img
+          onClick={() => navigate("/")}
+          src={assets.logo}
+          className="w-26 ml-7 my-2 cursor-pointer "
+          alt=""
+        />
         <hr className="border-gray-300 mb-8" />
-        <MenuItems setSidebarOpen={setSidebarOpen}/>
+        <MenuItems setSidebarOpen={setSidebarOpen} />
+        <Link
+          to="/create-post"
+          className="flex items-center justify-center gap-2 py-2.5 mt-6 mx-6 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95 transition text-white cursor-pointer"
+        >
+          <CirclePlus className="w-5 h-5" />
+          Create Post
+        </Link>
+      </div>
+
+      <div className="w-full border-t border-gray-200 p-4 px-7 flex items-center justify-between">
+        <div className="flex gap-2 items-center cursor-pointer">
+          <UserButton />
+          <div className="">
+            <h1 className="text-sm font-medium">{user.full_name}</h1>
+            <p className="text-xs text-gray-500">@{user.username}</p>
+          </div>
+        </div>
+        <LogOut onClick={signOut} className="W-4.5 text-gray-400 hover:text-gray-700 transition cursor-pointer" />
       </div>
     </div>
   );
