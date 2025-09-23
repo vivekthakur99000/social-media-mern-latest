1f57cfd1fe8f0c0e887bd4e40c1307fe279e3a20
vivek thakur <vivekthakur99000@gmail.com>
2025-09-23 19:53:40 +0530

    refactor MenuItems and StoryModel components; update icon usage and enhance story creation UI

--- client/src/components/StoryModel.jsx
+++ client/src/components/StoryModel.jsx
@@ -1,7 +1,7 @@
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, TextIcon, Upload } from "lucide-react";
 import React, { useState } from "react";
 
-const StoryModel = ({setShowModel, fetchStories}) => {
+const StoryModel = ({ setShowModel, fetchStories }) => {
   const bgColors = [
     "#4f46e5",
     "#7c3aed",
@@ -30,15 +30,59 @@ const StoryModel = ({setShowModel, fetchStories}) => {
 
   return (
     <div className="fixed inset-0 z-110 min-h-screen bg-black/80 backdrop-blur text-white flex items-center justify-center p-4 ">
-        <div className="w-full max-w-md">
-            <div className="text-center mb-4 flex items-center justify-between">
-                <button onClick={() => setShowModel(false)} className="text-white p-2 cursor-pointer ">
-                    <ArrowLeft />
-                </button>
-                <h2 className="text-lg font-semibold">Create Story</h2>
-                <span className="w-10"></span>
-            </div>
+      <div className="w-full max-w-md">
+        <div className="text-center mb-4 flex items-center justify-between">
+          <button
+            onClick={() => setShowModel(false)}
+            className="text-white p-2 cursor-pointer "
+          >
+            <ArrowLeft />
+          </button>
+          <h2 className="text-lg font-semibold">Create Story</h2>
+          <span className="w-10"></span>
         </div>
+        <div
+          className="rounded-lg h-96 flex items-center justify-center relative"
+          style={{ backgroundColor: background }}
+        >
+          {mode === "text" && (
+            <textarea
+              className="bg-transparent text-white w-full h-full p-6 text-lg resize-none focus:outline-none"
+              placeholder="What's on your mind"
+              onChange={(e) => setText(e.target.value)}
+              value={text}
+            />
+          )}
+          {mode === "media" && 
+            previewUrl &&
+            (media?.type.startsWith("image") ? (
+              <img src={previewUrl} className="object-contain max-w-full" />
+            ) : (
+              <video src={previewUrl} className="object-contain max-h-full" />
+            ))}
+        </div>
+
+        <div className="flex mt-4 gap-2">
+          {bgColors.map((color) => (
+            <button
+              key={color}
+              className="w-6 h-6 rounded-full ring cursor-pointer"
+              style={{ backgroundColor: color }}
+              onClick={() => setBackground(color)}
+            />
+          ))}
+        </div>
+
+        <div className="flex gap-2 mt-4">
+          <button onClick={() => {setMode("text"); setMedia(null); setPreviewUrl(null);}} className={`flex-1 flex items-center justify-center gap-2 p-2 rounded ${mode === 'text' ? "bg-white text-black" : "bg-zinc-800" }`}>
+            <TextIcon size={18} /> Text
+          </button>
+          <label className={`flex-1 flex items-center justify-center gap-2 p-2 rounded cursor-pointer ${mode === 'media' ? "bg-white text-black" : "bg-zinc-800"}` }>
+            <input onChange={(e) => {handleMediaUpload(e); setMode("media")}} type="file" accept="image/*, video/*" className="hidden" />
+            <Upload size={18} /> Photo/video
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
