e2ae59902262956db0c80c6f1a968484c255b437
vivek thakur <vivekthakur99000@gmail.com>
2025-10-08 19:33:48 +0530

    enhance PostCard component with like, comment, and share actions; add content rendering and image display features; update Feed component layout for sponsored content; create RecentMessages component skeleton

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import { BadgeCheck } from "lucide-react"
+import React, { useState } from 'react'
+import { BadgeCheck, Heart, MessageCircle, Share2 } from "lucide-react"
 import moment from 'moment'
+import { dummyUserData } from '../assets/assets'
 
 const PostCard = ({post}) => {
+
+  const postWithHashtags = post.content.replace(/(#\w+)/g, '<span class = "text-indigo-600">$1</span>')
+  const [likes, setLikes] = useState(post.likes_count)
+  const currentUser = dummyUserData
+  
+  const handleLike = async () => {
+    
+  }
+
   return (
     <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
       {/* user info */}
@@ -19,6 +29,36 @@ const PostCard = ({post}) => {
             </div>
         </div>
       </div>
+      {/* content */}
+      {
+        post.content && <div  className='text-gray-800 text-sm whitespace-pre-line' dangerouslySetInnerHTML={{__html : postWithHashtags}} />
+      }
+      {/* images */}
+      <div className="grid grid-cols-2 gap-2">
+        {
+          post.image_urls.map((img, index) => (
+            <img src={img} key={index} className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length === 1 && 'col-span-2 h-auto'}`} alt="" />
+          ))
+        }
+      </div>
+
+      {/* actions */}
+
+      <div className="flex items-center gap-4 text-gray-600 text-sm pt-2 border-t border-gray-300">
+        <div className="flex items-center gap-1">
+          <Heart className={`w-4 h-4 cursor-pointer ${likes.includes(currentUser._id) && 'text-red-500'}`} onClick={handleLike} />
+          <span>{likes.length}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <MessageCircle className="h-4 w-4" onClick={handleLike} />
+          <span>{12}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <Share2 className="h-4 w-4" onClick={handleLike} />
+          <span>{7}</span>
+        </div>
+      </div>
+
     </div>
   )
 }
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import PostCard from '../components/PostCard'
@@ -33,9 +33,13 @@ const Feed = () => {
         </div>
       </div>
       {/* right sidebar */}
-      <div className="">
-        <div className="">
-          <h1>Sponsered</h1>
+      <div className="max-xl:hidden sticky top-0">
+        <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
+          <h3 className='text-slate-800 font-semibold'>Sponsered</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p className='text-slate-600'>Email marketing</p>
+          <p className='text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nesciunt ipsum repellendus!</p>
+          
         </div>
         <h1>Recent messages</h1>
       </div>
