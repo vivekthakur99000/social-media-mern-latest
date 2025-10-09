d874f366197d5b90369d6de574508a43aaabc4b8
vivek thakur <vivekthakur99000@gmail.com>
2025-10-09 16:44:30 +0530

    enhance PostCard and RecentMessages components with navigation; update Messages page layout and user interaction features

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { BadgeCheck, Heart, MessageCircle, Share2 } from "lucide-react"
 import moment from 'moment'
 import { dummyUserData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const PostCard = ({post}) => {
 
@@ -13,10 +14,12 @@ const PostCard = ({post}) => {
     
   }
 
+  const navigate = useNavigate()
+
   return (
     <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
       {/* user info */}
-      <div className="inline-flex items-center gap-3 cursor-pointer">
+      <div onClick={() => navigate("/profile/" +post.user._id)} className="inline-flex items-center gap-3 cursor-pointer">
         <img src={post.user.profile_picture} className='w-10 h-10 rounded-full shadow' alt="" />
         <div className="">
             <div className="flex items-center space-x-1">
