4d98de5b92e34252195168a6cf7e959b68a186dd
vivek thakur <vivekthakur99000@gmail.com>
2025-10-18 19:00:19 +0530

    enhance Inngest integration; implement user creation, update, and deletion functions

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -10,37 +10,60 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const {id, first_name, last_name, email_addresses, image_url} = event.data
-    let username = email_addresses[0].email_address.split('@')[0]
-
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    let username = email_addresses[0].email_address.split("@")[0];
 
     // check availability of username
 
-    const user = await User.findOne({username})
+    const user = await User.findOne({ username });
 
     if (user) {
-        username += username + Math.floor(Math.random() * 10000)
+      username += username + Math.floor(Math.random() * 10000);
     }
 
     const userData = {
-        _id : id,
-        email : email_addresses[0].email_address,
-        full_name : first_name + " " + last_name,
-        profile_picture : image_url,
-        username
-    }
+      _id: id,
+      email: email_addresses[0].email_address,
+      full_name: first_name + " " + last_name,
+      profile_picture: image_url,
+      username,
+    };
 
-    await User.create(userData)
-
-
-  },
+    await User.create(userData);
+  }
 );
 
 // inngest function to update user data to db
 
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
 
+    const updatedUserData = {
+      email: email_addresses[0].email_address,
+      full_name: first_name + " " + last_name,
+      profile_picture: image_url,
+    };
+
+    await User.findByIdAndUpdate(id, updatedUserData);
+  }
+);
+
+// inngest function to delete the user from the db
+
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-from-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await User.findByIdAndDelete(id);
+  }
+);
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [
-    syncUserCreation
-];
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import 'dotenv/config'
 import connectDB from "./configs/db.js"
+import { serve } from "inngest/express";
 import {inngest, functions} from "./inngest/index.js"
 
 const app = express()
